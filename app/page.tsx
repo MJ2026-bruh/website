b13fad7245400dca1a266ac2611be9386224af56
Nouzen <shotoscpf@gmail.com>
2026-03-17 15:46:36 +0800

    Initial commit of website files

---
+++ app/page.tsx
@@ -0,0 +1,22 @@
+import FAQsThree from "@/components/faqs-3";
+import Features from "@/components/features-3";
+import FooterSection from "@/components/footer";
+import HeroSection from "@/components/hero-section";
+import IntegrationsSection from "@/components/integrations-3";
+import StatsSection from "@/components/stats";
+import TeamSection from "@/components/team";
+
+export default function Home() {
+  return (
+    <div> 
+      <HeroSection />
+      <Features />
+      <IntegrationsSection />
+      <StatsSection />
+      <TeamSection />
+      <FAQsThree />
+      <FooterSection />
+    
+    </div>
+    );
+}
